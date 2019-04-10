f94c7f7a963432151cc5d75460df84a2f2959135
Thomas Yoho <TYohoJr@Gmail.com>
2019-04-10 11:24:54 -0500

    integrated redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
 import NavigationBar from './Components/NavigationBar';
+import CurrentPage from './CurrentPage';
 
-class App extends Component {
+const store = createStore(reducer);
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavigationBar />
-        Hello
+      <Provider store={store}>
+        <div className="App">
+          <NavigationBar />
+          <CurrentPage />
       </div>
+      </Provider>
     );
   }
 }
-
-export default App;
