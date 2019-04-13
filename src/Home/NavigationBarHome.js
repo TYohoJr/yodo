dc2ebf54948d909eac3b38b9adf25391691818a6
Thomas Yoho <TYohoJr@Gmail.com>
2019-04-13 15:47:48 -0500

    formatted tabs and aligned content on screen

--- src/Home/NavigationBarHome.js
+++ src/Home/NavigationBarHome.js
@@ -1,39 +1,87 @@
 import React from 'react';
 import './Home.css';
 import { Nav, NavItem, NavLink } from 'reactstrap';
+import classnames from 'classnames';
 import { connect } from 'react-redux';
 import Transactions from './Transactions';
+import Accounts from './Accounts';
+import Budgets from './Budgets';
+import Reminders from './Reminders';
+import Investment from './Investment';
+import StockMarket from './StockMarket';
 
 class NavigationBarHome extends React.Component {
     constructor() {
         super();
-        this.showTransactions = this.showTransactions.bind(this);
+        this.changeCurrentTab = this.changeCurrentTab.bind(this);
+        this.state = {
+            activeTab: '1'
+        }
     }
 
-    showTransactions(tab) {
-        console.log(tab)
+    changeCurrentTab(page, tab) {
+        if(this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            })
+        }
         this.props.dispatch({
             type: 'changeHomePageTab',
-            currentTab: tab
-        })
+            currentTab: page
+        });
     }
 
     render() {
         return (
             <div>
-                <p>List Based</p>
                 <Nav tabs vertical>
                     <NavItem>
-                        <NavLink onClick={() => this.showTransactions(<Transactions />)}>Transactions</NavLink>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '1' })}
+                            onClick={() => { this.changeCurrentTab(<Transactions />, '1'); }}
+                        >
+                            Transactions
+                        </NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink href="#">Link</NavLink>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '2' })}
+                            onClick={() => { this.changeCurrentTab(<Budgets />, '2'); }}
+                        >
+                            Budgets
+                        </NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink href="#">Another Link</NavLink>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '3' })}
+                            onClick={() => { this.changeCurrentTab(<Accounts />, '3'); }}
+                        >
+                            Accounts
+                        </NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink href="#">Disabled Link</NavLink>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '4' })}
+                            onClick={() => { this.changeCurrentTab(<Reminders />, '4' ); }}
+                        >
+                            Reminders
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '5' })}
+                            onClick={() => { this.changeCurrentTab(<Investment />, '5'); }}
+                        >
+                            Investment
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '6' })}
+                            onClick={() => { this.changeCurrentTab(<StockMarket />, '6'); }}
+                        >
+                            Stock Market
+                        </NavLink>
                     </NavItem>
                 </Nav>
             </div>
